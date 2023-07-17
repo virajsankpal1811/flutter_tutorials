9168566f05b4b595b19e56bfa53a6bb8e54d44da
virajsankpal1811 <virajsankpal@nimapinfotech.com>
2023-07-17 12:05:39 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      // this is home
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
