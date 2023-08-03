d697f297443e5ddffe2eec1e657c8e07ac16ce09
virajsankpal1811 <virajsankpal@nimapinfotech.com>
2023-08-03 18:31:04 +0530

    New Commit

--- flutter_3/lib/main.dart
+++ flutter_3/lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 
 void main() {
@@ -80,6 +81,69 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
 
       )
+    );
+  }
+}
+
+
+ */
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title:'Hello'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          children: [
+            Container(
+              width: 120,
+              height: 100,
+              color: Colors.blueGrey,
+            )
+          ],
+        )
     );
   }
 }
